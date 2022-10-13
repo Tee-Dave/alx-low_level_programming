601c930d5a4227d5f93b9912d5120b7673eff021
Arowolo David <Teedave2203@gmail.com>
2022-10-13 07:14:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
- *
- ** main - printing
- *
- **
- *
- ** Return: Always 0 (Success)
- *
- **/
-
+ * main - Printing
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+		puts("\"Programming is like building a multilingual puzzle");
 
-	puts("\"Programming is like building a multilingual puzzle")
-	    return (0);
-
+			return (0);
 }
