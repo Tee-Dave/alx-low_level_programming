d2194ac39bce982f3a767e7088e6f74dded3608c
Arowolo David <Teedave2203@gmail.com>
2022-10-18 05:52:43 -0700

    edit prototype task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: prints 10 times the alphabet, in lowercase,
- * followed by a new line.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  *
  * Return: Always 0 (success)
  */
