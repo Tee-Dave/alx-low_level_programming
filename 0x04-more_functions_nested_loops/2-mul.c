a2770de1cf43bff51441a612db7aa1e1d1341590
Arowolo David <Teedave2203@gmail.com>
2022-10-20 17:13:09 -0700

    task 2 redo

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,14 @@
 #include"main.h"
 
 /**
- * print_numbers - print 0 - 9
- *             only using _putchar twice
+ * mul - multiplies two integers
+ * @a: first input
+ * @b: second input
  *
  * Return: Always 0 (Success)
  */
 
-void print_numbers(void)
+int mul(int a, int b)
 {
-	int num = 0;
-
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
-	_putchar('\n');
+	return (a * b);
 }
